e805ed496504dad39ea739435b8989b8f94eca0a
Rohan Oza <rohanoza@Rohans-MacBook-Air.local>
2025-07-07 16:00:37 +0530

    Fix: use correct field name for saving Editor text

--- text_editor/urls.py
+++ text_editor/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('save_text/', views.save_text, name='save_text'),
+    path('show_data/', views.show_data, name='show_data'),
 ]
--- text_editor/views.py
+++ text_editor/views.py
@@ -1,6 +1,18 @@
+from tkinter import YES
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def index(request):
     return render(request, 'text_editor/index.html')
+
+def save_text(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        print(title, description)
+        data = Editor.objects.create(title=title, text=description)
+    return render(request, 'text_editor/show.html')
+
+def show_data(request):
+    return render(request, 'text_editor/show.html')
